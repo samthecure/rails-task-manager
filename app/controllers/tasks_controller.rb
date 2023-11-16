720eaa37ada75442961a64c5f7c168b0de506cf1
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2023-11-17 00:58:29 +0100

    sauvegarde avant destructionrepo en ligne

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,18 +12,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    @task = Task.new
   end
 end
+#     if @task.save
+#       redirect_to tasks_path, notice: 'Task was successfully created.'
+#     else
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def task_params
+#     params.require(:task).permit(:title, :details, :completed)
+#   end
+# end
