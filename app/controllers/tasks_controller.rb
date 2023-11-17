7a9d7e612e577fffe737ed1550520084a1bf0bc4
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2023-11-17 02:50:18 +0100

    fin de l'exo sans bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
@@ -12,19 +13,37 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
-#     if @task.save
-#       redirect_to tasks_path, notice: 'Task was successfully created.'
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def task_params
-#     params.require(:task).permit(:title, :details, :completed)
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # root "posts#index"
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'task/create', to: 'tasks#create'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :new_edit
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
